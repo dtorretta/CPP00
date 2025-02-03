767b9ea54e210801d6e4014361596511872639f0
Daniela Torretta <dtorrett@c4b5c1.42berlin.de>
2025-02-03 20:06:57 +0100

    fixing

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -4,24 +4,20 @@
 
 int main(int ac, char **av)
 {
-	int i = 0;
-	size_t j = -1;
+	int i;
+	int j = 0;
 	
 	if (ac == 1)
 	{
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
-	
-	while(++i < ac)
+	while(av[++j])
 	{
-		std::string str = av[i]; 
-		while(++j < str.size())
-			std::cout << (char)std::toupper(str.at(j)); 
+		i = -1;
+		while(av[j][++i])
+			std::cout << (char)toupper(av[j][i]); 
 	}
 	std::cout << std::endl;
 	return (0);
 }
-
-
-//at() lanza una excepción std::out_of_range si intentas acceder a un índice fuera de los límites. simpelmente es una seguridad para hacer el codigo mas robusto
